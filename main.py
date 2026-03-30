cc709c1edd1137bb8e35180f33f0a91248876a65
Posirca Alexandru <76157684+Alexposh@users.noreply.github.com>
2026-03-30 18:08:29 +0100

    image animation and weapon placement

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 from enemy import Enemy
 from world import World
 from turret import Turret
+from button import Button
 import constants as c
 
 #initialise pygame
@@ -15,10 +16,18 @@ clock = pg.time.Clock()
 screen = pg.display.set_mode((c.SCREEN_WIDTH + c.SIDE_PANEL, c.SCREEN_HEIGHT))
 pg.display.set_caption("Tower Defence")
 
+
+# game variables 
+
+placing_turrets = False
+
 #load images
 
 #map
 map_image = pg.image.load('assests/levels/level.png').convert_alpha() 
+
+# turret spritesheet
+turret_sheet = pg.image.load('assests/turrets/turret_set_fire.png').convert_alpha()
 
 #individual turret image for mouse cursor
 cursor_turret = pg.image.load('assests/turrets/turret1.png').convert_alpha()
@@ -27,8 +36,8 @@ cursor_turret = pg.image.load('assests/turrets/turret1.png').convert_alpha()
 enemy_image = pg.image.load('assests/images/enemies/enemy_1.png').convert_alpha()
 
 # buttons
-buy_turret_image = pg.image.load('assests/turrets/turret1.png').convert_alpha()
-cancel_image = pg.image.load('assests/turrets/turret1.png').convert_alpha()
+buy_turret_image = pg.image.load('assests/images/buttons/buy.png').convert_alpha()
+cancel_image = pg.image.load('assests/images/buttons/cancel.png').convert_alpha()
 
 #load jon data for level
 with open('assests/levels/mapdata1.tmj') as file:
@@ -44,7 +53,7 @@ def create_turret(pos):
 
     # check if that tile is grass
     if world.tile_map[mouse_tile_num] == 25:
-        # CHECK if there is alreadya turret there
+        # CHECK if there is already a turret there
         space_is_free = True
         for turret in turret_group:
             if (mouse_pos) == (turret.pos): 
@@ -52,7 +61,7 @@ def create_turret(pos):
                 print('turret already there')
         # if this a free space, create a turret 
         if space_is_free:
-            new_turret = Turret(cursor_turret, mouse_pos)
+            new_turret = Turret(turret_sheet, mouse_pos)
             turret_group.add(new_turret)
             print('new turret')
 
@@ -67,25 +76,56 @@ turret_group = pg.sprite.Group()
 enemy = Enemy(world.waypoints, enemy_image)
 enemy_group.add(enemy)
 
+# create Button
+turret_button = Button(c.SCREEN_WIDTH + 55, 100, buy_turret_image, True)
+cancel_button = Button(c.SCREEN_WIDTH + 50, 180, cancel_image, True)
+
+
 run = True
 while run:
 
     clock.tick(c.FPS)
 
-    screen.fill("grey100")
+    ###################################
+    # UPDATING SECTION 
+    ###################################
+
+    # update groups
+    enemy_group.update() 
+    turret_group.update()
+
+    ###################################
+    # DRAWING SECTION 
+    ###################################
+    
+    screen.fill("grey100") 
 
     # draw level
     world.draw(screen)
 
     #draw enemy path
-    pg.draw.lines(screen,"grey0", False, world.waypoints)
-
-    # update groups
-    enemy_group.update() 
+    # pg.draw.lines(screen,"grey0", False, world.waypoints)
 
     #draw groups
     enemy_group.draw(screen)
     turret_group.draw(screen)
+
+    # draw buttons 
+    # button for placing turrets
+    if turret_button.draw(screen):
+        # print("new turret")
+        placing_turrets = True
+    # if placing turrets, show the cancel button as well
+    if placing_turrets:
+        # show cursor as turret 
+        cursor_rect = cursor_turret.get_rect()
+        cursor_pos = pg.mouse.get_pos()
+        cursor_rect.center = cursor_pos
+        if cursor_pos[0] <= c.SCREEN_WIDTH:
+            screen.blit(cursor_turret, cursor_rect)
+
+        if cancel_button.draw(screen):
+           placing_turrets = False
 
     # Event handler
     for event in  pg.event.get():
@@ -96,7 +136,8 @@ while run:
         if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
             mouse_pos = pg.mouse.get_pos()
             if mouse_pos[0] < c.SCREEN_WIDTH and mouse_pos[1] < c.SCREEN_HEIGHT:
-                create_turret(mouse_pos)                
+                if placing_turrets:
+                    create_turret(mouse_pos)                
 
     # update_display
     pg.display.flip()
--- turret.py
+++ turret.py
@@ -1,9 +1,54 @@
 import pygame as pg
+import constants as c
 
 class Turret(pg.sprite.Sprite):
-    def __init__(self, image, pos):
+    def __init__(self, sprite_sheet, pos):
         pg.sprite.Sprite.__init__(self)
-        self.image = image
+        self.cooldown = 1500
+        self.last_shot = pg.time.get_ticks()
+
+        # position variables
+
+
+        # animation variables
+
+        self.sprite_sheet = sprite_sheet
+        self.animation_list = self.load_images()
+        self.frame_index = 0
+        self.update_time = pg.time.get_ticks()
+
+        # update image      
+        self.image = self.animation_list[self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = pos
         self.pos = pos
+
+    def load_images(self):
+        # extract images from spritesheet
+        size = self.sprite_sheet.get_height()
+        animation_list = []
+        for x in range(c.ANIMATION_STEPS):
+            temp_img = self.sprite_sheet.subsurface(x * size, 0, size, size)
+            animation_list.append(temp_img)
+        return animation_list
+
+    def update(self):
+        # search for new target once turret cools down
+        if pg.time.get_ticks() - self.last_shot > self.cooldown:
+            self.play_animation()
+
+    def play_animation(self):
+        # update image
+        self.image = self.animation_list[self.frame_index]
+        # check if enought ime has passed saince the last update
+        if pg.time.get_ticks() - self.update_time > c.ANIMATION_DELAY:
+            self.update_time = pg.time.get_ticks()
+            self.frame_index += 1
+            # check if the animation has finished and reset to idle
+            if self.frame_index >= len(self.animation_list):
+                self.frame_index = 0
+                # record completed time and clear target so cooldown can begin
+                self.last_shot = pg.time.get_ticks()
+
+
+        
